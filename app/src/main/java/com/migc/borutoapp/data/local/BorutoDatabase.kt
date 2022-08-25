403eb8dd82d77311e4f4e3bdb2b46674c6e01b1e
Miguel <migc.dev@gmail.com>
2022-08-25 12:33:19 -0700

    created database

---
+++ app/src/main/java/com/migc/borutoapp/data/local/BorutoDatabase.kt
@@ -0,0 +1,13 @@
+package com.migc.borutoapp.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.migc.borutoapp.data.local.dao.HeroDao
+import com.migc.borutoapp.domain.model.Hero
+
+@Database(entities = [Hero::class], version = 1)
+abstract class BorutoDatabase : RoomDatabase() {
+
+    abstract fun heroDao(): HeroDao
+
+}
--- app/src/main/java/com/migc/borutoapp/utils/Constants.kt
+++ app/src/main/java/com/migc/borutoapp/utils/Constants.kt
@@ -3,5 +3,5 @@ package com.migc.borutoapp.utils
 object Constants {
     const val DETAILS_ARGUMENT_KEY = "heroId"
 
-    const val HERO_DATABASE_TABLE = ""
+    const val HERO_DATABASE_TABLE = "hero_table"
 }
