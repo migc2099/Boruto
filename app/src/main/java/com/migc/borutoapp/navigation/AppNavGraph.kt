a8d084193f6e4c46f70ca47762d756dfcecf5aa2
Miguel <migc.dev@gmail.com>
2022-08-25 18:19:30 -0700

    implemented splash screen

--- app/src/main/java/com/migc/borutoapp/navigation/AppNavGraph.kt
+++ app/src/main/java/com/migc/borutoapp/navigation/AppNavGraph.kt
@@ -1,12 +1,12 @@
 package com.migc.borutoapp.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.migc.borutoapp.presentation.screen.splash.SplashScreen
 import com.migc.borutoapp.utils.Constants.DETAILS_ARGUMENT_KEY
 
 @Composable
@@ -16,7 +16,7 @@ fun AppNavGraph(navController: NavHostController) {
         startDestination = Screen.Splash.route
     ) {
         composable(route = Screen.Splash.route) {
-
+            SplashScreen(navController = navController)
         }
         composable(route = Screen.Welcome.route) {
 
