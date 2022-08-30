d897bbe4f7b4409aab8c728c59fc692a267d913c
Miguel <migc.dev@gmail.com>
2022-08-30 11:24:36 -0700

    added content of bottom sheet

--- app/src/main/java/com/migc/borutoapp/presentation/screens/details/DetailsContent.kt
+++ app/src/main/java/com/migc/borutoapp/presentation/screens/details/DetailsContent.kt
@@ -1,28 +1,32 @@
 package com.migc.borutoapp.presentation.screens.details
 
-import android.util.Log
-import androidx.annotation.MainThread
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.migc.borutoapp.R
 import com.migc.borutoapp.domain.model.Hero
 import com.migc.borutoapp.presentation.components.InfoBox
 import com.migc.borutoapp.presentation.components.OrderedList
 import com.migc.borutoapp.ui.theme.*
 import com.migc.borutoapp.utils.Constants.ABOUT_TEXT_MAX_LINES
-import kotlinx.coroutines.*
-import kotlin.coroutines.coroutineContext
+import com.migc.borutoapp.utils.Constants.BASE_URL
 
 @ExperimentalMaterialApi
 @Composable
@@ -45,12 +49,64 @@ fun DetailsContent(
             }
         },
         content = {
-            Box() {
-
+            selectedHero?.let { hero ->
+                BackgroundContent(
+                    heroImage = hero.image,
+                    onCloseClicked = {
+                        navHostController.popBackStack()
+                    }
+                )
             }
         }
     )
 
+}
+
+@Composable
+fun BackgroundContent(
+    heroImage: String,
+    imageFraction: Float = 1f,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    onCloseClicked: () -> Unit
+) {
+    val imageUrl = "$BASE_URL${heroImage}"
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("$BASE_URL${heroImage}")
+                .placeholder(R.drawable.ic_placeholder)
+                .error(R.drawable.ic_placeholder)
+                .build(),
+            contentDescription = stringResource(R.string.hero_image),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = imageFraction)
+                .align(Alignment.TopStart)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            IconButton(
+                modifier = Modifier.padding(all = SMALL_PADDING),
+                onClick = { onCloseClicked() }
+            ) {
+                Icon(
+                    modifier = Modifier.size(32.dp),
+                    imageVector = Icons.Default.Close,
+                    contentDescription = stringResource(id = R.string.close_icon),
+                    tint = Color.White
+                )
+            }
+        }
+    }
 }
 
 @Composable
